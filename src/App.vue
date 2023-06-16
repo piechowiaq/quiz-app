66f2289121b80cecf1ac48d188d9f7e3d3ee26b8
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-06-16 21:22:13 +0200

    Getting json data

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup>
+
+import q from "./data/quizes.json"
+import {ref} from "vue";
+
+const quizes = ref(q)
+
+</script>
+
 <template>
   <div class="container">
     <header>
@@ -5,11 +14,11 @@
       <input type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div class="card">
-        <img  src="" alt="">
+      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img :src="quiz.img" alt="">
         <div class="card-text">
-          <h2>Math</h2>
-          <p>15 questions</p>
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
         </div>
       </div>
     </div>
