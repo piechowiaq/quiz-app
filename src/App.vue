ffbab7705aeda93d15bea53ae16d9c151b75d4e1
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-06-17 20:51:57 +0200

    Insulating the component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 
 import q from "./data/quizes.json"
-import {ref, watch} from "vue";
+import {ref, watch} from "vue"
+import Card from "./components/Card.vue"
 
 const quizes = ref(q)
 const search = ref("")
@@ -19,13 +20,14 @@ watch(search, () => {
       <input v-model.trim="search"  type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="">
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+<!--      <div v-for="quiz in quizes" :key="quiz.id" class="card">-->
+<!--        <img :src="quiz.img" alt="">-->
+<!--        <div class="card-text">-->
+<!--          <h2>{{ quiz.name }}</h2>-->
+<!--          <p>{{ quiz.questions.length }} questions</p>-->
+<!--        </div>-->
+<!--      </div>-->
     </div>
   </div>
 </template>
@@ -62,32 +64,6 @@ header input {
   margin-top: 40px;
 }
 
-/*CARD*/
 
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
-
-}
 
 </style>
